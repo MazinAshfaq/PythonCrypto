b9fb0b5034d810ad9a1295d678f9b1bfad617e59
Mazin Ashfaq <mzash00@gmail.com>
2021-11-11 14:51:26 -0800

    Add previous files and create model_datastore.py

---
+++ hw4/gbmodel/model_datastore.py
@@ -0,0 +1,42 @@
+from .Model import Model
+from datetime import datetime
+from google.cloud import datastore
+
+def from_datastore(entity):
+    """Translates Datastore results into the format expected by the
+    application.
+
+    Datastore typically returns:
+        [Entity{key: (kind, id), prop: val, ...}]
+
+    This returns:
+        [ name, email, date, message ]
+    where name, email, and message are Python strings
+    and where date is a Python datetime
+    """
+    if not entity:
+        return None
+    if isinstance(entity, list):
+        entity = entity.pop()
+    return [entity['name'],entity['email'],entity['date'],entity['message']]
+
+class model(Model):
+    def __init__(self):
+        self.client = datastore.Client('YOUR_PROJECT_ID')
+
+    def select(self):
+        query = self.client.query(kind = 'HW4')
+        entities = list(map(from_datastore,query.fetch()))
+        return entities
+
+    def insert(self,name,email,message):
+        key = self.client.key('Review')
+        rev = datastore.Entity(key)
+        rev.update( {
+            'name': name,
+            'email' : email,
+            'date' : datetime.today(),
+            'message' : message
+            })
+        self.client.put(rev)
+        return True
---
+++ hw4/gbmodel/model_sqlite3.py
@@ -0,0 +1,65 @@
+"""
+Data is stored in a SQLite database that looks something like the following:
+
++------------+------------------+------------+----------------+--------------+----------+
+| Department | Course Number    | Quarter    | Year           | Instructor   | Review   |
++============+==================+============+----------------+--------------+----------+
+| CS         | CS430            | Fall       | 2021           | Wu-Chang     | Great!   |
++------------+------------------+------------+----------------+--------------+----------+
+
+This is created with the following SQL:
+
+    create table coursereviews (department text, coursenumber integer, quarter text, year integer, instructor text, review text);
+
+"""
+from datetime import date
+from .Model import Model
+import sqlite3
+DB_FILE = 'entries.db'    # file for our Database
+
+class model(Model):
+    def __init__(self):
+
+        # Make sure our database exists
+        connection = sqlite3.connect(DB_FILE)
+        print("setting connetion to DB_FILE")
+        cursor = connection.cursor()
+        try:
+            cursor.execute("select count(department) from coursereviews")
+            print("Trying to count database")
+        except sqlite3.OperationalError:
+            cursor.execute("create table coursereviews (department text, coursenumber integer, quarter text, year integer, instructor text, review text)")
+            print("CREATED TABLE IF NOT CREATED ALREADY")
+        cursor.close()
+
+    def select(self):
+        """
+        Gets all rows from the database
+        Each row contains: department, coursenumber, quarter, year, instructor, review
+        :return: List of lists containing all rows of database
+        """
+        connection = sqlite3.connect(DB_FILE)
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM coursereviews")
+        return cursor.fetchall()
+
+    def insert(self, department, coursenumber, quarter, year, instructor, review):
+        """
+        Inserts entry into database
+        :param department: String
+        :param coursenumber: Integer
+        :param quarter: String
+        :param year: Integer
+        :param instructor: String
+        :param review: String
+        :return: True
+        :raises: Database errors on connection and insertion
+        """
+        params = {'department':department, 'coursenumber':coursenumber, 'quarter':quarter, 'year':year, 'instructor':instructor, 'review':review }
+        connection = sqlite3.connect(DB_FILE)
+        cursor = connection.cursor()
+        cursor.execute("insert into coursereviews (department, coursenumber, quarter, year, instructor, review) VALUES (:department, :coursenumber, :quarter, :year, :instructor, :review)", params)
+
+        connection.commit()
+        cursor.close()
+        return True
