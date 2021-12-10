38e9bf82789d573a43a6908d67b9b91870912895
Mazin Ashfaq <mzash00@gmail.com>
2021-12-10 08:16:08 -0800

    Created base.html and imported bootstrap globally

--- final/app.py
+++ final/app.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    return "HomePage"
+
+@app.route("/Charts")
+def chart():
     data = functions.fetchData()
-    return render_template("home.html", btc = data[0], eth=data[1])
+    return render_template("chart.html", btc = data[0], eth=data[1])
 
 
 if __name__ == "__main__":
