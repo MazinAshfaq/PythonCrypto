36360a7d5838152598f18e093c5c3885e0bbbf5b
Mazin Ashfaq <mzash00@gmail.com>
2021-12-11 17:20:02 -0800

    removing other finals

--- hw3/index.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from flask.views import MethodView
-import gbmodel
-
-class Index(MethodView):
-    def get(self):
-        model = gbmodel.get_model()
-        entries = [dict(department=row[0], coursenumber=row[1], quarter=row[2], year=row[3], instructor=row[4],review=row[5]) for row in model.select()]
-        return render_template('index.html',entries=entries)
